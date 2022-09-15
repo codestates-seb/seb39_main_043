4df50426afcd938a724beb0a75165789456dde53
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 15:55:53 +0900

    front/style : InputLoginPassword css border 추가

--- client/src/components/atoms/InputLoginPassword.js
+++ client/src/components/atoms/InputLoginPassword.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const InputLoginPasswordWrapper = styled.div`
   width: 440px;
   height: 120px;
+  border: 1px solid black;
 `;
 
 const InputLoginPassword = () => {
