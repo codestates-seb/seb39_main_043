3956d42e840ccbb4dc0018d9811282758cf60311
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 15:46:30 +0900

    front/feat : InputLoginId 컴포넌트 기능 추가

---
+++ client/src/components/atoms/InputLoginId.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const InputLoginIdWrapper = styled.div`
+  width: 440px;
+  height: 120px;
+`;
+
+const InputLoginId = () => {
+  return (
+    <InputLoginIdWrapper>
+      <input></input>
+    </InputLoginIdWrapper>
+  );
+};
+
+export default InputLoginId;
