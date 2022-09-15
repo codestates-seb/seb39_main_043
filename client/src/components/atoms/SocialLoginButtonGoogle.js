10f1ec72a941ed4dea512e3e00ff6e691ad3deb0
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 17:02:11 +0900

    front/feat : SocialLoginButtonGoogle css 수정

--- client/src/components/atoms/SocialLoginButtonGoogle.js
+++ client/src/components/atoms/SocialLoginButtonGoogle.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
 
 const SocialLoginButtonGoogleWrapper = styled.div`
-  width: 442px;
+  width: 440px;
   height: 60px;
   border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .btn-name {
+    font-size: 24px;
+  }
 `;
 
 const SocialLoginButtonGoogle = () => {
-  return <SocialLoginButtonGoogleWrapper>구글</SocialLoginButtonGoogleWrapper>;
+  return (
+    <SocialLoginButtonGoogleWrapper>
+      <span className="btn-name">(IMG) 구글</span>
+    </SocialLoginButtonGoogleWrapper>
+  );
 };
 
 export default SocialLoginButtonGoogle;
