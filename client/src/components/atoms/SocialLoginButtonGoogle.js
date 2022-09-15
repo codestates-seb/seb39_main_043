b87f2b39ed50aea02214d781aa779200b2366007
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 15:54:22 +0900

    front/feat : SocialLoginButtonGoogle 컴포넌트 기능 추가

---
+++ client/src/components/atoms/SocialLoginButtonGoogle.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SocialLoginButtonGoogleWrapper = styled.div`
+  width: 442px;
+  height: 60px;
+  border: 1px solid black;
+`;
+
+const SocialLoginButtonGoogle = () => {
+  return <SocialLoginButtonGoogleWrapper>구글</SocialLoginButtonGoogleWrapper>;
+};
+
+export default SocialLoginButtonGoogle;
