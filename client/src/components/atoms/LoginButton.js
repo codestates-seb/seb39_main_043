65bf296033af0909b1178975666a7987c179d323
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 15:59:15 +0900

    front/feat : LoginButton 컴포넌트 기능 추가

---
+++ client/src/components/atoms/LoginButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const LoginButtonWrapper = styled.div`
+  width: 442px;
+  height: 60px;
+`;
+
+const LoginButton = () => {
+  return <LoginButtonWrapper>로그인</LoginButtonWrapper>;
+};
+
+export default LoginButton;
