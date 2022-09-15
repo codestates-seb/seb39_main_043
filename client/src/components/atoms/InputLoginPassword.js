6260b61c9feb654326b4ab6b2473998a942b80a8
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 15:48:14 +0900

    front/feat : InputLoginPassword 컴포넌트 기능 추가

---
+++ client/src/components/atoms/InputLoginPassword.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const InputLoginPasswordWrapper = styled.div`
+  width: 440px;
+  height: 120px;
+`;
+
+const InputLoginPassword = () => {
+  return (
+    <InputLoginPasswordWrapper>
+      <input></input>
+    </InputLoginPasswordWrapper>
+  );
+};
+
+export default InputLoginPassword;
