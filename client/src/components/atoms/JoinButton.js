dfa959b0511587c60d148c7397e02f409e0bb3f1
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-15 16:03:13 +0900

    front/feat : JoinButtin 컴포넌트 기능 추가

---
+++ client/src/components/atoms/JoinButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const JoinButtonWrapper = styled.div`
+  width: 442px;
+  height: 60px;
+  border: 1px solid black;
+`;
+
+const JoinButton = () => {
+  return <JoinButtonWrapper>회원가입</JoinButtonWrapper>;
+};
+
+export default JoinButton;
