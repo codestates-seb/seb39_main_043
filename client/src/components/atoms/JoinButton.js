128321861f4761fc1d76ac542ae61452fee92a6b
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 15:01:44 +0900

    front/feat : JoinButton 컴포넌트 마크업 구현

--- client/src/components/atoms/JoinButton.js
+++ client/src/components/atoms/JoinButton.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
 
 const JoinButtonWrapper = styled.div`
-  width: 442px;
+  width: 440px;
   height: 60px;
   border: 1px solid black;
+  border-radius: 10px;
+  background-color: #d9d9d9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .join-text {
+    font-size: 24px;
+  }
 `;
 
 const JoinButton = () => {
-  return <JoinButtonWrapper>회원가입</JoinButtonWrapper>;
+  return (
+    <JoinButtonWrapper>
+      <span className="join-text">회원가입</span>
+    </JoinButtonWrapper>
+  );
 };
 
 export default JoinButton;
