af1d55550ed948320446a66cd18a18e518a8b90f
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 14:58:53 +0900

    front/feat : LoginButton 컴포넌트 마크업 구현

--- client/src/components/atoms/LoginButton.js
+++ client/src/components/atoms/LoginButton.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
 
 const LoginButtonWrapper = styled.div`
-  width: 442px;
+  width: 440px;
   height: 60px;
   border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #d9d9d9;
+  .login-text {
+    font-size: 24px;
+  }
 `;
 
 const LoginButton = () => {
-  return <LoginButtonWrapper>로그인</LoginButtonWrapper>;
+  return (
+    <LoginButtonWrapper>
+      <span className="login-text">로그인</span>
+    </LoginButtonWrapper>
+  );
 };
 
 export default LoginButton;
