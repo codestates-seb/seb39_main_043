1e88d07c691e9b1b9966ee3063c9daac5ee02b2a
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 14:29:49 +0900

    front/feat : InputLoginId 컴포넌트 기능 구현

--- client/src/components/atoms/InputLoginId.js
+++ client/src/components/atoms/InputLoginId.js
@@ -1,15 +1,32 @@
 import styled from 'styled-components';
+import { CgProfile } from 'react-icons/cg';
 
 const InputLoginIdWrapper = styled.div`
   width: 440px;
   height: 120px;
   border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  background-color: #f5f5f5;
+
+  .login-id {
+    width: 360px;
+    height: 60px;
+    border: none;
+    background-color: #f5f5f5;
+    font-size: 24px;
+    margin-left: 10px;
+  }
 `;
 
 const InputLoginId = () => {
   return (
     <InputLoginIdWrapper>
-      <input></input>
+      <CgProfile size={40} />
+      <input className="login-id" placeholder="아이디를 입력하세요"></input>
     </InputLoginIdWrapper>
   );
 };
