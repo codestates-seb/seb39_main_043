9e2680da664bc4c949ef9f784c1a21c66c883e1b
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 16:48:16 +0900

    front/feat : ScheduleCreateButton 컴포넌트 마크업 구현

---
+++ client/src/components/atoms/ScheduleCreateButton.js
@@ -0,0 +1,7 @@
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+
+const ScheduleCreateButton = () => {
+  return <AiOutlinePlusCircle size={60} color={'#007FDB'} />;
+};
+
+export default ScheduleCreateButton;
