d1db85911ab273ed65d257d5550b51b21c026674
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 16:45:00 +0900

    front/feat : LogoIcon 컴포넌트 마크업 구현

---
+++ client/src/components/atoms/LogoIcon.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const LogoIconWrapper = styled.div`
+  width: 80px;
+  height: 40px;
+  text-align: center;
+  font-size: 14px;
+`;
+
+const LogoIcon = () => {
+  return <LogoIconWrapper>Social Calendar</LogoIconWrapper>;
+};
+
+export default LogoIcon;
