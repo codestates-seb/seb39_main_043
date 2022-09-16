d833e399e3cbc4835e34a1f2a64110546b1f64ec
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 16:16:02 +0900

    front/feat : CurrentCalendarTitle 컴포넌트 마크업 구현

---
+++ client/src/components/atoms/CurrentCalendarTitle.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const CurrentCalendarTitleWrapeer = styled.div`
+  font-size: 16px;
+
+  .current-calendar-text {
+    font-weight: bold;
+  }
+`;
+
+const CurrentCalendarTitle = () => {
+  return (
+    <CurrentCalendarTitleWrapeer>
+      <span className="current-calendar-text">현재 캘린더</span>
+      <span>{' 스터디'}</span>
+    </CurrentCalendarTitleWrapeer>
+  );
+};
+
+export default CurrentCalendarTitle;
