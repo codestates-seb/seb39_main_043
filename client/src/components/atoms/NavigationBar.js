425965e7ce2379208ab78ec3472f4e4599c64004
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-16 15:38:45 +0900

    front/feat : NavigationBar 컴포넌트 마크업 구현

---
+++ client/src/components/atoms/NavigationBar.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const NavigationBarWrapper = styled.div`
+  width: 1280px;
+  height: 50px;
+  background-color: #f5f5f5;
+`;
+
+const NavigationBar = () => {
+  return <NavigationBarWrapper></NavigationBarWrapper>;
+};
+
+export default NavigationBar;
