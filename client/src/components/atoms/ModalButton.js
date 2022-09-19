0699476eb22da7507671498ef28ec2e7a5aa46cf
mimi <hongmikyeong11@gmail.com>
2022-09-19 13:53:40 +0900

    front/refactor : 중복되는 버튼 컴포넌트를 모달 버튼으로 통합

---
+++ client/src/components/atoms/ModalButton.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const ModalButtonWrapper = styled.button`
+  width: 80px;
+  height: 30px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  font-weight: bold;
+  font-size: 16px;
+
+  background-color: ${(props) => props.color};
+
+  &:hover {
+    filter: drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.5));
+  }
+
+  &:active {
+    filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.5));
+  }
+`;
+
+const ModalButton = ({ color, value }) => {
+  return <ModalButtonWrapper color={color}>{value}</ModalButtonWrapper>;
+};
+
+export default ModalButton;
