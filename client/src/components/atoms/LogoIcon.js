5d9fbfaff7c0fd8c01de817ad3684f856f21f97e
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-20 09:43:59 +0900

    front/css,fix : LogoIcon 컴포넌트 css 수정 및 스타일 확장을 위한 className props 를 받을 수 있도록 수정

--- client/src/components/atoms/LogoIcon.js
+++ client/src/components/atoms/LogoIcon.js
@@ -3,12 +3,23 @@ import styled from 'styled-components';
 const LogoIconWrapper = styled.div`
   width: 80px;
   height: 40px;
-  text-align: center;
   font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .logo-icon {
+    text-align: center;
+    line-height: 1;
+  }
 `;
 
-const LogoIcon = () => {
-  return <LogoIconWrapper>Social Calendar</LogoIconWrapper>;
+const LogoIcon = ({ className }) => {
+  return (
+    <LogoIconWrapper className={className}>
+      <div className="logo-icon">Social Calendar</div>
+    </LogoIconWrapper>
+  );
 };
 
 export default LogoIcon;
