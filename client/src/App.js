0aa737de63bfc9fbae6a64fe02b7f92439d16b8c
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-20 16:59:07 +0900

    front/chore : App.js 초기화

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import JoinForm from './components/molecules/JoinForm';
 import LoginForm from './components/molecules/LoginForm';
+import MyPageNavigation from './components/molecules/MyPageNavigation';
+import MainPageNavigation from './components/molecules/MainPageNavigation';
+import Calendar from './components/molecules/Calendar';
 
 function App() {
-  return <LoginForm></LoginForm>;
+  return (
+    <>
+      <LoginForm />
+      <JoinForm />
+      <MyPageNavigation />
+      <MainPageNavigation year={2022} month={9} />
+      <Calendar />
+    </>
+  );
 }
 
 export default App;
