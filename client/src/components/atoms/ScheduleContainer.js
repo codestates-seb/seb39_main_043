30a010a317c1275d931d68d7e5a30375547e1510
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-21 13:34:51 +0900

    front/css : height 하드 코딩에서 auto 로 변경

--- client/src/components/atoms/ScheduleContainer.js
+++ client/src/components/atoms/ScheduleContainer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const ScheduleContainerWrapper = styled.div`
   width: 183px;
-  height: 183px;
+  height: auto;
   border: 1px solid black;
   display: flex;
   flex-direction: column;
