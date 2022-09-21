a086172f1540b8e8bc2eebe74aa8b06db84a413c
mimi <hongmikyeong11@gmail.com>
2022-09-21 18:23:05 +0900

    front/refactor : 메인페이지에서 커스텀 스타일링을 할 수 있도록 props 추가 (className)

--- client/src/components/atoms/MypageSidebarContainer.js
+++ client/src/components/atoms/MypageSidebarContainer.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const Conatiner = styled.div`
   width: 180px;
+  background-color: white;
   border: 1px solid #d5d5d5;
 `;
 
-const MypageSidebarContainer = ({ children }) => {
-  return <Conatiner>{children}</Conatiner>;
+const MypageSidebarContainer = ({ className, children }) => {
+  return <Conatiner className={className}>{children}</Conatiner>;
 };
 
 export default MypageSidebarContainer;
