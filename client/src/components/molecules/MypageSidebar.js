d66267e672e5662448b19553a6e4a8356277d4b0
mimi <hongmikyeong11@gmail.com>
2022-09-23 14:53:51 +0900

    front/refactor: 캘린더 사이드바, 마이페이지 사이드바 이동을 위한 Link 기능 추가

--- client/src/components/molecules/MypageSidebar.js
+++ client/src/components/molecules/MypageSidebar.js
@@ -1,11 +1,17 @@
 import atoms from "../atoms";
+import { Link } from "react-router-dom";
 
 const MypageSidebar = ({ className }) => {
   return (
     <atoms.MypageSidebarContainer className={className}>
       <atoms.MypageSidebarNickname content={"appleBanana"} />
-      <atoms.MypageSidebarItem content={"내 정보 수정"} />
-      <atoms.MypageSidebarItem content={"캘린더 관리"} />
+      <Link to="/mypage/myinfopage">
+        <atoms.MypageSidebarItem content={"내 정보 수정"} />
+      </Link>
+
+      <Link to="/mypage/mycalendarpage">
+        <atoms.MypageSidebarItem content={"캘린더 관리"} />
+      </Link>
       <atoms.MypageSidebarLogout />
     </atoms.MypageSidebarContainer>
   );
