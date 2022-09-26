e31a439123d3571908e6d0fde04c0698d3dd1452
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-26 13:59:46 +0900

    front/fix : 회원가입 비밀번호를 전역상태로 관리할 수 있도록 수정

--- client/src/components/atoms/InputJoinPassword.js
+++ client/src/components/atoms/InputJoinPassword.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { IoKeyOutline } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
+import joinSlice from '../../slices/joinSlice';
 
 const InputJoinPasswordWrapper = styled.div`
   width: 440px;
@@ -16,17 +18,27 @@ const InputJoinPasswordWrapper = styled.div`
     width: 360px;
     height: 40px;
     margin-left: 10px;
-    font-size: 24px;
-    border: none;
-    background-color: #f9f9f9;
+
+    input {
+      font-size: 24px;
+      border: none;
+      background-color: #f9f9f9;
+      height: 40px;
+    }
   }
 `;
 
-const InputJoinPassword = () => {
+const InputJoinPassword = ({ children }) => {
+  const dispatch = useDispatch();
+  // let password = useSelector((state) => state.join.password);
+  // console.log('password', password);
   return (
     <InputJoinPasswordWrapper>
       <IoKeyOutline size={40} />
-      <input className="join-password" placeholder="비밀번호를 입력해주세요" type="password"></input>
+      <div className="join-password">
+        <input placeholder="비밀번호를 입력해주세요" type="password" onChange={(e) => dispatch(joinSlice.actions.join({ password: e.target.value }))}></input>
+        {children}
+      </div>
     </InputJoinPasswordWrapper>
   );
 };
