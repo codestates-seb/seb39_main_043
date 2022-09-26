7035e5e3b1e073a8d80af717cc12afa250460926
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-26 13:56:09 +0900

    front/feat : JoinForm에 warningBox 추가

--- client/src/components/molecules/JoinForm.js
+++ client/src/components/molecules/JoinForm.js
@@ -1,14 +1,20 @@
-import styled from "styled-components";
-import atoms from "../atoms";
+import styled from 'styled-components';
+import atoms from '../atoms';
 
 const JoinFormWrapper = styled.div``;
 
 const JoinForm = ({ className }) => {
   return (
     <JoinFormWrapper className={className}>
-      <atoms.InputJoinNickname />
-      <atoms.InputJoinId />
-      <atoms.InputJoinPassword />
+      <atoms.InputJoinNickname>
+        <atoms.WarningBox className={''} value={'이미 존재하는 별명입니다.'} />
+      </atoms.InputJoinNickname>
+      <atoms.InputJoinId>
+        <atoms.WarningBox className={''} value={'5~12자의 영문 소문자, 숫자만 사용 가능 합니다.'} />
+      </atoms.InputJoinId>
+      <atoms.InputJoinPassword>
+        <atoms.WarningBox className={''} value={'8~16자의 영문 대 소문자, 숫자, 특수기호를 사용하세요'} />
+      </atoms.InputJoinPassword>
     </JoinFormWrapper>
   );
 };
