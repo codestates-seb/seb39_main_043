267fda3e8af51b31d012012ea2a03ed1f17ad4c8
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-26 13:52:01 +0900

    front/fix : userSlice, joinSlice reducer 추가

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dateSlice from './slices/dateSlice';
+import joinSlice from './slices/joinSlice';
+import userSlice from './slices/userSlice';
 
 const store = configureStore({
   reducer: {
     date: dateSlice.reducer,
+    user: userSlice.reducer,
+    join: joinSlice.reducer,
   },
 });
 
