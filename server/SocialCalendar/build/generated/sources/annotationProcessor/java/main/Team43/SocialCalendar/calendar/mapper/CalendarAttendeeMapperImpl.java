e335d34a9cbf391925e78655684c5a909e6fe6bc
JungGyu Ahn(안중규) <newending01@gmail.com>
2022-09-27 16:58:15 +0900

    빌드

---
+++ server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/calendar/mapper/CalendarAttendeeMapperImpl.java
@@ -0,0 +1,13 @@
+package Team43.SocialCalendar.calendar.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-09-27T16:56:25+0900",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.15 (Azul Systems, Inc.)"
+)
+@Component
+public class CalendarAttendeeMapperImpl implements CalendarAttendeeMapper {
+}
---
+++ server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/schedule/mapper/ScheduleMapperImpl.java
@@ -0,0 +1,77 @@
+package Team43.SocialCalendar.schedule.mapper;
+
+import Team43.SocialCalendar.schedule.dto.SchedulePatchDto;
+import Team43.SocialCalendar.schedule.dto.SchedulePostDto;
+import Team43.SocialCalendar.schedule.dto.ScheduleResponseDto;
+import Team43.SocialCalendar.schedule.entity.Schedule;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-09-27T16:56:25+0900",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.15 (Azul Systems, Inc.)"
+)
+@Component
+public class ScheduleMapperImpl implements ScheduleMapper {
+
+    @Override
+    public Schedule schedulePostDtoToSchedule(SchedulePostDto schedulePostDto) {
+        if ( schedulePostDto == null ) {
+            return null;
+        }
+
+        Schedule schedule = new Schedule();
+
+        schedule.setTitle( schedulePostDto.getTitle() );
+        schedule.setAttendees( schedulePostDto.getAttendees() );
+        schedule.setScheduleAt( schedulePostDto.getScheduleAt() );
+        schedule.setLocation( schedulePostDto.getLocation() );
+        schedule.setContents( schedulePostDto.getContents() );
+
+        return schedule;
+    }
+
+    @Override
+    public Schedule schedulePatchDtoToSchedule(SchedulePatchDto schedulePatchDto) {
+        if ( schedulePatchDto == null ) {
+            return null;
+        }
+
+        Schedule schedule = new Schedule();
+
+        schedule.setScheduleId( schedulePatchDto.getScheduleId() );
+        schedule.setTitle( schedulePatchDto.getTitle() );
+        schedule.setAttendees( schedulePatchDto.getAttendees() );
+        schedule.setScheduleAt( schedulePatchDto.getScheduleAt() );
+        schedule.setLocation( schedulePatchDto.getLocation() );
+        schedule.setContents( schedulePatchDto.getContents() );
+
+        return schedule;
+    }
+
+    @Override
+    public ScheduleResponseDto scheduleToScheduleResponseDto(Schedule schedule) {
+        if ( schedule == null ) {
+            return null;
+        }
+
+        Long scheduleId = null;
+        String title = null;
+        String scheduleAt = null;
+        String attendees = null;
+        String location = null;
+        String contents = null;
+
+        scheduleId = schedule.getScheduleId();
+        title = schedule.getTitle();
+        scheduleAt = schedule.getScheduleAt();
+        attendees = schedule.getAttendees();
+        location = schedule.getLocation();
+        contents = schedule.getContents();
+
+        ScheduleResponseDto scheduleResponseDto = new ScheduleResponseDto( scheduleId, title, scheduleAt, attendees, location, contents );
+
+        return scheduleResponseDto;
+    }
+}
