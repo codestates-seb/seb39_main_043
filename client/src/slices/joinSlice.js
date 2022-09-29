1e778e653c50ab3d457cf8fbe9d92a359de9a55a
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-29 14:58:07 +0900

    front/refactor : 초기 상태의 속성명 변경

--- client/src/slices/joinSlice.js
+++ client/src/slices/joinSlice.js
@@ -2,11 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const joinSlice = createSlice({
   name: 'join',
-  initialState: { nickname: undefined, id: undefined, email: undefined, password: undefined },
+  initialState: { name: undefined, email: undefined, password: undefined },
   reducers: {
     join: (state, action) => {
-      state.nickname = action.payload.nickname;
-      state.id = action.payload.id;
+      state.name = action.payload.name;
       state.email = action.payload.email;
       state.password = action.payload.password;
     },
