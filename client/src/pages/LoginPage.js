d321e29f2b745fb21c66ca15605f1bbc15061740
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-29 14:57:07 +0900

    front/feat : 로그인 정보 입력 후 로그인 버튼 클릭시 DB의 회원정보와 비교하여 일치하는 정보가 있으면 로그인 후 메인페이지 이동, 없으면 경고메세지 렌더링하는 기능 구현

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 import atoms from '../components/atoms';
 import molecules from '../components/molecules';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import warningSlice from '../slices/warningSlice';
+import userSlice from '../slices/userSlice';
 
 const LoginPageWrapper = styled.div`
   display: flex;
@@ -41,18 +46,48 @@ const SocialLoginButtonGoogle = styled(atoms.SocialLoginButtonGoogle)`
 `;
 
 const LoginPage = () => {
+  const user = useSelector((state) => state.user);
+  const loginWarning = useSelector((state) => state.warning.loginWarning);
+  const dispatch = useDispatch();
+
+  const { data, isLoading, isError, error } = useQuery('login', async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members`);
+    return data;
+  });
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError)
+    return (
+      <>
+        <h1>Something is wrong</h1>
+        <p>{error.toString()}</p>
+      </>
+    );
+  if (!data) return <div></div>;
+  if (data) console.log('data : ', data);
+
+  const isUser = (email, password, userData) => {
+    let result = userData.filter((el) => el.email === email && el.password === password);
+    console.log('result', result);
+    if (result.length === 0) {
+      dispatch(warningSlice.actions.log({ loginWarning: '' }));
+    } else {
+      dispatch(userSlice.actions.user({ name: result[0].name, id: result[0].memberId, email: result[0].email, password: result[0].password }));
+    }
+  };
+
   return (
     <LoginPageWrapper>
       <LogoIcon />
       <LoginForm />
-      <WarningBox className={'hidden'} value={`아이디 또는 비밀번호를 잘못 입력했습니다.\n입력한 내용을 다시 확인해주세요.`} />
-      <LoginButton color={'#007FDB'} />
+      <WarningBox className={loginWarning} value={`아이디 또는 비밀번호를 잘못 입력했습니다.\n입력한 내용을 다시 확인해주세요.`} />
+      <LoginButton color={'#007FDB'} onClick={() => isUser(user.email, user.password, data)} />
       <Line />
       <SocialLoginButtonGoogle />
       <div className="join-link">
         <span>아직 회원이 아니신가요? </span>
         <Link to="/joinpage">회원가입 하기</Link>
       </div>
+      {user.id && <Navigate to="/mainpage" replace={true} />}
     </LoginPageWrapper>
   );
 };
