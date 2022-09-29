f64597e05f2089bad0980b922ca9869dd5fb89d2
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-09-29 15:02:26 +0900

    front/feat : warningSlice reducer 추가

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import dateSlice from './slices/dateSlice';
 import joinSlice from './slices/joinSlice';
 import userSlice from './slices/userSlice';
-import myInfoSlice from "./slices/myPage";
+import myInfoSlice from './slices/myPage';
+import warningSlice from './slices/warningSlice';
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     user: userSlice.reducer,
     join: joinSlice.reducer,
     myInfo: myInfoSlice.reducer,
+    warning: warningSlice.reducer,
   },
 });
 
