6e9f099e69c5944745520dc2e8c1dddf371953ef
mimi <hongmikyeong11@gmail.com>
2022-10-04 13:01:16 +0900

    front/chore : redux persist 설치 및 적용

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,53 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dateSlice from './slices/dateSlice';
-import joinSlice from './slices/joinSlice';
-import userSlice from './slices/userSlice';
-import myInfoSlice from './slices/myPage';
-import warningSlice from './slices/warningSlice';
-import modalSlice from './slices/modalSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import dateSlice from "./slices/dateSlice";
+import joinSlice from "./slices/joinSlice";
+import userSlice from "./slices/userSlice";
+import myInfoSlice from "./slices/myPage";
+import warningSlice from "./slices/warningSlice";
+import modalSlice from "./slices/modalSlice";
+import calendarSlice from "./slices/calendarSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    date: dateSlice.reducer,
-    user: userSlice.reducer,
-    join: joinSlice.reducer,
-    myInfo: myInfoSlice.reducer,
-    warning: warningSlice.reducer,
-    modal: modalSlice.reducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  date: dateSlice.reducer,
+  user: userSlice.reducer,
+  join: joinSlice.reducer,
+  myInfo: myInfoSlice.reducer,
+  warning: warningSlice.reducer,
+  modal: modalSlice.reducer,
+  calendar: calendarSlice.reducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+// const store = configureStore({
+//   reducer: {
+//     date: dateSlice.reducer,
+//     user: userSlice.reducer,
+//     join: joinSlice.reducer,
+//     myInfo: myInfoSlice.reducer,
+//     warning: warningSlice.reducer,
+//     modal: modalSlice.reducer,
+//     calendar: calendarSlice.reducer,
+//   },
+// });
 
 export default store;
