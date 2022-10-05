48a92d247b9c01401da77d2c305074a5a482dbcf
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-10-05 10:19:21 +0900

    front/fix : diaryModal, createDiaryModal 상태 추가

--- client/src/slices/modalSlice.js
+++ client/src/slices/modalSlice.js
@@ -10,6 +10,8 @@ const modalSlice = createSlice({
     eventModal: false,
     eventCommentModal: false,
     inviteAttendeeModal: false,
+    diaryModal: false,
+    createDiaryModal: false,
   },
   reducers: {
     modal: (state, action) => {
@@ -20,6 +22,8 @@ const modalSlice = createSlice({
       state.eventModal = action.payload.eventModal;
       state.mypageSidebarModal = action.payload.mypageSidebarModal;
       state.inviteAttendeeModal = action.payload.inviteAttendeeModal;
+      state.diaryModal = action.payload.diaryModal;
+      state.createDiaryModal = action.payload.createDiaryModal;
     },
   },
 });
