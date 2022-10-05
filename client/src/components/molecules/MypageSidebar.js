14d6897e089a1c2f5152d5065c7fc5085af5ccb1
mimi <hongmikyeong11@gmail.com>
2022-10-05 10:22:04 +0900

    front/refactor : 마이페이지에서 별명 수정 시 사이드바에 즉시 수정될 수 있도록 리팩토링

--- client/src/components/molecules/MypageSidebar.js
+++ client/src/components/molecules/MypageSidebar.js
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import modalSlice from "../../slices/modalSlice";
 import { persistor } from "../../store";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const MypageSidebar = ({ className }) => {
   const user = useSelector((state) => state.user);
@@ -10,25 +12,28 @@ const MypageSidebar = ({ className }) => {
   const dispatch = useDispatch();
   const modalState = useSelector((state) => state.modal);
 
-  // [함수] 로그아웃
-  // const logout = () => {
-  //   const result = window.confirm("로그아웃 하시겠습니까?");
-  //   if (result) {
-  //     dispatch(modalSlice.actions.modal({ ...modalState, mypageSidebarModal: false }));
-  //     navigate("/", { replace: true });
-  //     // (memo) navigate의 인자로 {replace:true}를 넣으면 navigate에 적힌 주소로 넘어간 후 뒤로가기를 하더라도 방금의 페이지로 돌아오지 못한다.
-  //     //        replace의 default 값은 false로 뒤로가기가 가능하다.
-  //   }
-  // };
-
+  // [기능] 로그아웃
   const purge = async () => {
-    await persistor.purge();
-    navigate("/", { replace: true });
+    const result = window.confirm("로그아웃 하시겠습니까?");
+    if (result) {
+      await persistor.purge();
+      dispatch(modalSlice.actions.modal({ ...modalState, mypageSidebarModal: false }));
+      navigate("/", { replace: true });
+    }
   };
+
+  // [기능] 사용자 정보 불러오기
+  const { isLoading, isError, error, data } = useQuery("userInfo", async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members/${user.id}`);
+    return data;
+  });
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>{error.message}</div>;
 
   return (
     <atoms.MypageSidebarContainer className={className}>
-      <atoms.MypageSidebarNickname content={user.name} />
+      <atoms.MypageSidebarNickname content={data.name} />
       <Link to="/mypage/myinfopage">
         <atoms.MypageSidebarItem content={"내 정보 수정"} />
       </Link>
@@ -37,7 +42,6 @@ const MypageSidebar = ({ className }) => {
         <atoms.MypageSidebarItem content={"캘린더 관리"} />
       </Link>
       <atoms.MypageSidebarLogout onClick={() => purge()} />
-      {/* <atoms.MypageSidebarLogout onClick={logout} /> */}
     </atoms.MypageSidebarContainer>
   );
 };
