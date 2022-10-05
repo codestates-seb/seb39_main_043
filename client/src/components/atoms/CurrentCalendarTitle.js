a29e82536a91717c1d150c4a27e9792045bffa90
mimi <hongmikyeong11@gmail.com>
2022-10-05 10:20:14 +0900

    front/refactor : 캘린더명 수정 시 네비게이션 바에 나타난 캘린더명이 즉시 수정될 수 있도록 리팩토링

--- client/src/components/atoms/CurrentCalendarTitle.js
+++ client/src/components/atoms/CurrentCalendarTitle.js
@@ -1,5 +1,9 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useEffect, useFocusEffect } from "react";
+import { useQuery } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import React from "react";
 
 const CurrentCalendarTitleWrapeer = styled.div`
   font-size: 16px;
@@ -12,10 +16,22 @@ const CurrentCalendarTitleWrapeer = styled.div`
 const CurrentCalendarTitle = ({ className }) => {
   const calendar = useSelector((state) => state.calendar);
 
+  const { isLoading, isError, error, data, refetch } = useQuery("calendarName", async () => {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/calendars/${calendar.id}`);
+    return data;
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [calendar.id]);
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>{error.message}</div>;
+
   return (
     <CurrentCalendarTitleWrapeer className={className}>
       <span className="current-calendar-text">현재 캘린더 </span>
-      <span>{calendar.title}</span>
+      <span>{data.title}</span>
     </CurrentCalendarTitleWrapeer>
   );
 };
