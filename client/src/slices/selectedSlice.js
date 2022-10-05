9b93ca5640a493f872cdd520c27cb1b4d8281b79
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-10-05 17:49:51 +0900

    front/feat : selected diaryId 추가

--- client/src/slices/selectedSlice.js
+++ client/src/slices/selectedSlice.js
@@ -2,11 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const selectedSlice = createSlice({
   name: 'selected',
-  initialState: { calendarId: 0, scheduleId: 3 },
+  initialState: { calendarId: 0, scheduleId: 0, diaryId: 0 },
   reducers: {
     selected: (state, action) => {
       state.calendarId = action.payload.calendarId;
       state.scheduleId = action.payload.scheduleId;
+      state.diaryId = action.payload.diaryId;
     },
   },
 });
