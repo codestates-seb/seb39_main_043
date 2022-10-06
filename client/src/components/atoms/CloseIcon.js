164e0259782a02f72ae72f58e284e5e54c34ab31
mimi <hongmikyeong11@gmail.com>
2022-10-06 21:09:43 +0900

    front/refactor : 일정 작성 모달의 네비게이션바에 위치한 아이콘 간격 수정

--- client/src/components/atoms/CloseIcon.js
+++ client/src/components/atoms/CloseIcon.js
@@ -1,7 +1,11 @@
 import { MdClose } from "react-icons/md";
 
 const CloseIcon = ({ id, onClick }) => {
-  return <MdClose id={id} onClick={onClick} size={20} />;
+  return (
+    <span>
+      <MdClose id={id} onClick={onClick} size={20} />
+    </span>
+  );
 };
 
 export default CloseIcon;
