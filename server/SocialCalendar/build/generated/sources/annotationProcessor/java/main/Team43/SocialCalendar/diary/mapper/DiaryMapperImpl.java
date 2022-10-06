879e485e2bf2caa5a3470d1ef957bc913866b4ed
JungGyu Ahn(안중규) <newending01@gmail.com>
2022-10-06 12:42:48 +0900

    diaryInfo가 null일 때 0으로 응답하게끔 구현

--- server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/diary/mapper/DiaryMapperImpl.java
+++ server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/diary/mapper/DiaryMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-05T14:49:16+0900",
+    date = "2022-10-06T12:40:00+0900",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.15 (Azul Systems, Inc.)"
 )
 @Component
