1a42b73eb377a7fa097869349afa70bf1767a446
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-10-06 19:22:25 +0900

    front/fix : 캘린더 선택 시 selectedState update 되도록 수정

--- client/src/components/molecules/CalendarSidebar.js
+++ client/src/components/molecules/CalendarSidebar.js
@@ -1,11 +1,12 @@
-import axios from "axios";
-import { useQuery } from "react-query";
-import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
-import atoms from "../atoms";
-import calendarSlice from "../../slices/calendarSlice";
-import { useNavigate } from "react-router-dom";
-import modalSlice from "../../slices/modalSlice";
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
+import atoms from '../atoms';
+import calendarSlice from '../../slices/calendarSlice';
+import { useNavigate } from 'react-router-dom';
+import modalSlice from '../../slices/modalSlice';
+import selectedSlice from '../../slices/selectedSlice';
 
 // <--- styled component --->
 const StyledCalendarProfile = styled(atoms.CalendarProfile)`
@@ -16,30 +17,44 @@ const StyledCalendarAddButton = styled(atoms.CalendarAddButton)`
   margin-bottom: 16px;
 `;
 
+const getCalendars = async (memberId) => {
+  const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members/${memberId}`);
+  return data.adminCalendars.concat(data.attendedCalendars);
+};
+
+const getUserInfo = async (memberId) => {
+  const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members/${memberId}`);
+  return data;
+};
 // <--- CalendarSideBar --->
 const CalendarSideBar = ({ className }) => {
-  const user = useSelector((state) => state.user);
-  const calendar = useSelector((state) => state.calendar);
+  const userState = useSelector((state) => state.user);
+  // const calendarState = useSelector((state) => state.calendar);
+  const selectedState = useSelector((state) => state.selected);
   const modalState = useSelector((state) => state.modal);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  console.log("캘린더 사이드바에서 calendar 상태 : ", calendar);
+  // console.log('캘린더 사이드바에서 calendar 상태 : ', calendarState);
 
   // [함수] 캘린더 목록 GET
-  const { isLoading, error, data } = useQuery("calendar", async () => {
-    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members/${user.id}`);
-    if (data.attendedCalendars) {
-      return data.adminCalendars.concat(data.attendedCalendars);
-    }
-    return data.adminCalendars;
-  });
+  const userInfo = useQuery('userInfo', () => getUserInfo(userState.id));
+  const calendars = useQuery('calendars', () => getCalendars(userState.id));
+  if (calendars.isLoading) return <h3>Loading...</h3>;
+  if (calendars.isError)
+    return (
+      <>
+        <h3>calendar sidebar error</h3>
+        <p>{calendars.error.toString()}</p>
+      </>
+    );
 
   // [함수] 참고하고 있는 캘린더 변경
-  const selectCalendar = (id, title) => {
+  const selectCalendar = (id, title, isAttendee) => {
     dispatch(calendarSlice.actions.setCalendar({ id, title }));
     dispatch(modalSlice.actions.modal({ ...modalState, calendarSidebarModal: false }));
-    navigate("/mainpage");
+    dispatch(selectedSlice.actions.selected({ ...selectedState, calendarId: id, isAttendee }));
+    navigate('/mainpage');
   };
 
   return (
@@ -50,11 +65,11 @@ const CalendarSideBar = ({ className }) => {
       </div>
 
       {/* 캘린더 영역 */}
-      {data &&
-        data.map((value) => (
-          <atoms.CalendarItemContainer key={value.calendarId} onClick={() => selectCalendar(value.calendarId, value.title)}>
-            <StyledCalendarProfile imgURL={""} />
-            <atoms.CalendarName content={value.title} />
+      {calendars.data &&
+        calendars.data.map((calendar) => (
+          <atoms.CalendarItemContainer key={calendar.calendarId} onClick={() => selectCalendar(calendar.calendarId, calendar.title, calendar.memberId !== userState.id)}>
+            <StyledCalendarProfile imgURL={''} />
+            <atoms.CalendarName content={calendar.title} />
           </atoms.CalendarItemContainer>
         ))}
     </atoms.CalendarSidebarContainer>
