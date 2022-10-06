7d0be71a54f21374ae380be3cf4b4ab5ebb96899
Kim-Hyunsoo-96-kr <kim.hyunsoo.96.kr@gmail.com>
2022-10-06 19:36:26 +0900

    front/fix : 마이페이지 이동 시 마이페이지 사이드바 닫히도록 수정

--- client/src/components/molecules/MypageSidebar.js
+++ client/src/components/molecules/MypageSidebar.js
@@ -1,10 +1,11 @@
-import atoms from "../atoms";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import modalSlice from "../../slices/modalSlice";
-import { persistor } from "../../store";
-import { useQuery } from "react-query";
-import axios from "axios";
+import atoms from '../atoms';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import modalSlice from '../../slices/modalSlice';
+import { persistor } from '../../store';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import selectedSlice from '../../slices/selectedSlice';
 
 const MypageSidebar = ({ className }) => {
   const user = useSelector((state) => state.user);
@@ -14,10 +15,11 @@ const MypageSidebar = ({ className }) => {
 
   // [기능] 로그아웃
   const purge = async () => {
-    const result = window.confirm("로그아웃 하시겠습니까?");
+    const result = window.confirm('로그아웃 하시겠습니까?');
     if (result) {
       dispatch(modalSlice.actions.modal({ ...modalState, mypageSidebarModal: false }));
-      navigate("/", { replace: true });
+      dispatch(selectedSlice.actions.selected({}));
+      navigate('/', { replace: true });
       setTimeout(async () => {
         await persistor.purge();
       }, 500);
@@ -25,7 +27,7 @@ const MypageSidebar = ({ className }) => {
   };
 
   // [기능] 사용자 정보 불러오기
-  const { isLoading, isError, error, data } = useQuery("userInfo", async () => {
+  const { isLoading, isError, error, data } = useQuery('userInfo', async () => {
     const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/members/${user.id}`);
     return data;
   });
@@ -37,11 +39,11 @@ const MypageSidebar = ({ className }) => {
     <atoms.MypageSidebarContainer className={className}>
       <atoms.MypageSidebarNickname content={data.name} />
       <Link to="/mypage/myinfopage">
-        <atoms.MypageSidebarItem content={"내 정보 수정"} />
+        <atoms.MypageSidebarItem content={'내 정보 수정'} />
       </Link>
 
       <Link to="/mypage/mycalendarpage">
-        <atoms.MypageSidebarItem content={"캘린더 관리"} />
+        <atoms.MypageSidebarItem content={'캘린더 관리'} />
       </Link>
       <atoms.MypageSidebarLogout onClick={purge} />
     </atoms.MypageSidebarContainer>
