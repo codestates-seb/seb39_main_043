f346981d11ad21a3ede8299b266b0d938953fe95
Woogiemon <96171524+Woogiemon@users.noreply.github.com>
2022-10-17 20:44:46 +0900

    Revert "Back"

--- server/SocialCalendar/src/main/java/Team43/SocialCalendar/diary/repository/DiaryRepository.java
+++
@@ -1,7 +0,0 @@
-package Team43.SocialCalendar.diary.repository;
-
-import Team43.SocialCalendar.diary.entity.Diary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryRepository extends JpaRepository<Diary, Long> {
-}
