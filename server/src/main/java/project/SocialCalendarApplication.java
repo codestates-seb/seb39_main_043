16671ac2c3bcaaf34c65ea5df98c8baef58fcc58
JIN <taschen94@gmail.com>
2022-10-20 14:14:30 +0900

    FIRST COMMIT

---
+++ server/src/main/java/project/SocialCalendarApplication.java
@@ -0,0 +1,12 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialCalendarApplication.class, args);
+	}
+}
