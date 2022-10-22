0b901d5e6971b84787f3eda80701a90e15a7ca41
JIN <taschen94@gmail.com>
2022-10-22 23:21:22 +0900

    back/feat: CORS 설정 추가

--- server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/calendar/mapper/CalendarMapperImpl.java
+++ server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/calendar/mapper/CalendarMapperImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     date = "2022-10-06T17:10:49+0900",
 =======
     date = "2022-09-27T18:37:08+0900",
@@ -26,6 +27,9 @@ import org.springframework.stereotype.Component;
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.15 (Azul Systems, Inc.)"
 =======
     date = "2022-10-04T21:18:51+0900",
+=======
+    date = "2022-10-05T19:00:41+0900",
+>>>>>>> 45b8282 (back/feat: CORS 설정 추가)
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.14.1 (Azul Systems, Inc.)"
 >>>>>>> 5ce170e (back/feat: Jwt 방식의 Login 구현)
 )
--- server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/schedule/comment/mapper/ScheduleCommentMapperImpl.java
+++ server/SocialCalendar/build/generated/sources/annotationProcessor/java/main/Team43/SocialCalendar/schedule/comment/mapper/ScheduleCommentMapperImpl.java
@@ -7,6 +7,7 @@ import javax.annotation.processing.Generated;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     date = "2022-10-06T17:10:49+0900",
 =======
     date = "2022-09-30T17:06:27+0900",
@@ -17,6 +18,9 @@ import javax.annotation.processing.Generated;
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.15 (Azul Systems, Inc.)"
 =======
     date = "2022-10-04T21:18:51+0900",
+=======
+    date = "2022-10-05T19:00:41+0900",
+>>>>>>> 45b8282 (back/feat: CORS 설정 추가)
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 11.0.14.1 (Azul Systems, Inc.)"
 >>>>>>> 5ce170e (back/feat: Jwt 방식의 Login 구현)
 )
